60a99d45c0c92633579b03fd254810e1e54b2561
Md. Shahariar H. Tanim <tanimshorkar77@gmail.com>
2025-07-21 01:18:42 +0600

    Create Logical Operators.c

---
+++ Logical Operators.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main()
+{
+    int a = 7, b = 7, c = 14, result;
+
+    result = (a == b) && (c > b);
+    printf("(a == b) && (c > b) is %d \n", result);
+
+    result = (a == b) && (c < b);
+    printf("(a == b) && (c < b) is %d \n", result);
+
+    result = (a == b) || (c < b);
+    printf("(a == b) || (c < b) is %d \n", result);
+
+    result = (a != b) || (c < b);
+    printf("(a != b) || (c < b) is %d \n", result);
+
+    result = !(a != b);
+    printf("!(a != b) is %d \n", result);
+
+    result = !(a == b);
+    printf("!(a == b) is %d \n", result);
+
+    return 0;
+}
