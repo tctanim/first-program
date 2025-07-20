131b250d78e62c718ac356e0534e0c9e374a2bcb
Md. Shahariar H. Tanim <tanimshorkar77@gmail.com>
2025-07-21 01:04:03 +0600

    Create Arithmetic Operators.c

---
+++ Arithmetic Operators.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main(){
+
+    int Tanim = 7081, Tonim =7080, Tutal;
+
+    Tutal =Tanim+Tonim;
+    printf("Tanim+Tonim =%d\n",Tutal);
+    Tutal =Tanim-Tonim;
+    printf("Tanim-Tonim =%d\n",Tutal);
+    Tutal =Tanim*Tonim;
+    printf("Tanim*Tonim =%d\n",Tutal);
+    Tutal =Tanim/Tonim;
+    printf("Tanim/Tonim =%d\n",Tutal);
+    Tutal =Tanim%Tonim;
+    printf("Remainder when Tanim divided by Tonim = %d \n",Tutal);
+
+
+
+
+
+
+  return 0;
+}
