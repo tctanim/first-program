1be8573c6ac0d25c12f08c5137bbd83c5a56d9ab
Md. Shahariar H. Tanim <tanimshorkar77@gmail.com>
2025-07-21 01:18:47 +0600

    Create Relational Operators.c

---
+++ Relational Operators.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    int a = 7, b = 7, c = 14;
+
+    printf("%d == %d is %d \n", a, b, a == b);
+    printf("%d == %d is %d \n", a, c, a == c);
+    printf("%d > %d is %d \n", a, b, a > b);
+    printf("%d > %d is %d \n", a, c, a > c);
+    printf("%d < %d is %d \n", a, b, a < b);
+    printf("%d < %d is %d \n", a, c, a < c);
+    printf("%d != %d is %d \n", a, b, a != b);
+    printf("%d != %d is %d \n", a, c, a != c);
+    printf("%d >= %d is %d \n", a, b, a >= b);
+    printf("%d >= %d is %d \n", a, c, a >= c);
+    printf("%d <= %d is %d \n", a, b, a <= b);
+    printf("%d <= %d is %d \n", a, c, a <= c);
+
+    return 0;
+}
