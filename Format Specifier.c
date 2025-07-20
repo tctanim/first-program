e8dd9972215668d7901f5226e27ec20b1852bc3f
Md. Shahariar H. Tanim <tanimshorkar77@gmail.com>
2025-07-20 19:35:08 +0600

    Create Format Specifier.c

---
+++ Format Specifier.c
@@ -0,0 +1,20 @@
+
+#include<stdio.h>
+
+  int main() {
+
+
+  int age =19;
+  double PI =3.14;
+  char unit ='C';
+  float GPA =3.58;
+
+
+  printf("Your Age: %d\n",age);
+  printf("PI : %lf\n",PI);
+  printf("Unit : %c\n",unit);
+  printf("GPA : %f",GPA);
+
+  return 0;
+
+  }
