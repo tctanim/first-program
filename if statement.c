deaefe7a05ccedc0cc2e6bdcff5261327749e31c
Md. Shahariar H. Tanim <tanimshorkar77@gmail.com>
2025-07-22 20:41:39 +0600

    Create if statement.c

---
+++ if statement.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main() {
+
+  int Mailston;
+   printf("Enter your class: ");
+   scanf("%d", &Mailston);
+
+    if (Mailston <=5){
+        printf(" Her class is %d.\n",Mailston);
+
+    }
+    printf(" This class is not you.");
+
+
+
+ return 0;
+}
