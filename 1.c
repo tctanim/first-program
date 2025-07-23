1b6c8ae35567a7f8a187b7c73810ec7241824b41
Md. Shahariar H. Tanim <tanimshorkar77@gmail.com>
2025-07-24 03:42:19 +0600

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+
+
+     char lowercase_char;
+     char uppercase_char;
+
+     while (lowercase_char == uppercase_char){
+     if ( lowercase_char ==lowercase_char){
+     printf("TTT =");
+     scanf(" %c", &lowercase_char);
+     char uppercase_char = toupper(lowercase_char);
+     printf("TTT =%c\n", uppercase_char);
+
+
+     }
+     printf("ttt ");
+    scanf(" %c", &uppercase_char);
+      char lowercase_char = tolower(uppercase_char);
+        printf("ttt= %c\n", lowercase_char);
+     }
+
+    return 0;
+}
