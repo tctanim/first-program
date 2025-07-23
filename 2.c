0af538eb961dc0aca47d597499cd2b61cf0b3ad3
Md. Shahariar H. Tanim <tanimshorkar77@gmail.com>
2025-07-24 03:42:23 +0600

    Create 2.c

---
+++ 2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdbool.h>
+int main(){
+
+    bool married =false;
+     printf("Status : ");
+     scanf("%d", &married);
+
+     if ( married){
+        printf("are you old.");
+     }
+     else {
+        printf("are you young.");
+     }
+
+return 0;
+}
