b62ce5061adfa1bc5bd5fd3c7c2f6e5cde14a5ae
Md. Shahariar H. Tanim <tanimshorkar77@gmail.com>
2025-09-02 19:34:13 -0700

    Create 9.c

---
+++ 9.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main (){
+int num;
+double time, time1;
+scanf(" %d",&num);
+scanf("%lf %lf",&time, &time1);
+printf("NUMBER = %d\n",num);
+double SALARY = (time * time1);
+printf("SALARY = U$ %.2lf\n",SALARY);
+
+return 0;
+}
